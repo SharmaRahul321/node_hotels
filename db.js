49e89b9f1d6d61333306e8e9371c1ba1b4cfdc59
SharmaRahul321 <rahulbeojya06082002@gmail.com>
2024-07-26 17:25:25 +0530

    mongodb again shift online

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 // const mongoURL = 'mongodb://127.0.0.1:27017/hotels';
 // const mongoURL= 'mongodb+srv://rahulberojya06082002:Rahul123@cluster0.fibrgbg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-const mongoURL= process.env.MONGODB_URL_LOCAL;
+// const mongoURL= process.env.MONGODB_URL_LOCAL;
 const mongoURL= process.env.MONGODB_URL;; 
 mongoose.connect(mongoURL, {
   useNewUrlParser: true,
